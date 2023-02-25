7d54555ab90c0bccd991af0e038d36e9a05ed8b1
MichaelM3 <michael.muniz94@gmail.com>
2023-02-25 14:54:52 -0500

    added some routes to app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return "Hello World!"
+@app.get("/blog")
+async def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published: 
+        return { "data": f"{limit} published blogs from the db" }
+    return { "data": f"{limit} blogs from the db" }
+
+@app.get("/blog/unpublished")
+async def unpublished():
+    return { "data": "All unpublished blogs" }
+
+@app.get("/blog/{id}")
+async def show(id: int):
+    return { "data": {id} }
+
+@app.get("/blog/{id}/comments")
+async def comments(id: int, limit: int = 10):
+    return { "data": {"1", "2"} }

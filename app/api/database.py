aa866906590c6b396457b93f20c25238678a734f
MichaelM3 <michael.muniz94@gmail.com>
2023-02-27 14:05:29 -0500

    fixed database file and started models file

--- app/api/database.py
+++ app/api/database.py
@@ -1,9 +1,17 @@
 from dotenv import load_dotenv
 load_dotenv()
+from os import getenv
 from sqlalchemy import create_engine
-import os
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL: str = os.getenv("DATABASE_URL") or ""
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("No DATABASE_URL variable found in .env")
 
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+Base = declarative_base()
 

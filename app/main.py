db56ddcfaccae44f27699fbd54c74352853cc5b4
MichaelM3 <michael.muniz94@gmail.com>
2023-02-28 17:31:02 -0500

    added delete route

--- app/main.py
+++ app/main.py
@@ -17,21 +17,29 @@ def get_db():
         db.close()
 
 @app.get("/blog")
-async def index(limit: int = 10, sort: Optional[str] = None, db: Session = Depends(get_db)):
+def index(limit: int = 10, sort: Optional[str] = None, db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 @app.get("/blog/{id}", status_code=status.HTTP_200_OK)
-async def show(id: int, res: Response, db: Session = Depends(get_db)):
+def show(id: int, res: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No blog found with id of {id}")
     return blog
 
 @app.post("/blog", status_code=status.HTTP_201_CREATED)
-async def create_blog(req: schemas.Blog, db: Session = Depends(get_db)):
+def create(req: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=req.title, body=req.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return "Blog was deleted"
+
+

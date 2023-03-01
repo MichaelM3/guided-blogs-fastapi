6bbce2faf8e87da84866708225916ebe7d164dca
MichaelM3 <michael.muniz94@gmail.com>
2023-02-28 23:06:42 -0500

    found temporary update solution

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,9 +1,20 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class BlogBase(BaseModel):
     title: str
     body: str
 
 class Blog(BlogBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class BlogCreate(BlogBase):
+    pass
+
+class BlogUpdate(BlogBase):
+    title: Optional[str] = None
+    body: Optional[str] = None
     class Config:
         orm_mode = True
--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ load_dotenv()
 from fastapi import Depends, FastAPI, HTTPException, status
 from .api import database, models, schemas
 from sqlalchemy.orm import Session
-from sqlalchemy import update
 
 app = FastAPI()
 
@@ -23,12 +22,13 @@ def index(db: Session = Depends(get_db)):
 
 @app.get("/blog/{id}", status_code=status.HTTP_200_OK)
 def show(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog with this id was not found!")
+    return blog
 
 @app.post("/blog", status_code=status.HTTP_201_CREATED)
-def create(req: schemas.Blog, db: Session = Depends(get_db)):
+def create(req: schemas.BlogCreate, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=req.title, body=req.body)
     db.add(new_blog)
     db.commit()
@@ -36,17 +36,15 @@ def create(req: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update(id: int, req: schemas.Blog, db: Session = Depends(get_db)):
-    db.execute(
-       update(models.Blog),
-            )
-    # blog = db.query(models.Blog).where(models.Blog.id == id)
-    # if not blog.first():
-    #     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog with this id was not found!")
-    # blog.update(req.dict(exclude_unset=True))
-    # db.commit()
-    # db.refresh(blog)
-    # return blog
+def update(id: int, req: schemas.BlogUpdate, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Blog with this id was not found!")
+    for key, value in req.dict(exclude_unset=True).items():
+        setattr(blog, key, value)
+    db.commit()
+    db.refresh(blog)
+    return blog
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id: int, db: Session = Depends(get_db)):

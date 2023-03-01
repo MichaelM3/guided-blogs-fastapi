58b78a26ea7e923e931ce8256a07b8062ae48b8a
MichaelM3 <michael.muniz94@gmail.com>
2023-03-01 18:17:02 -0500

    fixed requirements file and updated token schema

--- app/database/schemas.py
+++ app/database/schemas.py
@@ -32,3 +32,10 @@ class BlogUpdate(BlogBase):
 class Login(BaseModel):
     email: str
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
---
+++ app/routers/token.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+from os import getenv
+from typing import Optional
+from jose import jwt
+
+JWT_SECRET = getenv("JWT_SECRET")
+if not JWT_SECRET:
+    raise ValueError("Environment Variable for JWT_SECRET not set!")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=ALGORITHM)
+
+    return encoded_jwt

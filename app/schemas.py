26f2fc6de8b03705cf4e40d57b5f022e63b76aa8
MichaelM3 <michael.muniz94@gmail.com>
2023-03-01 14:26:35 -0500

    added schema relationships

--- app/schemas.py
+++ app/schemas.py
@@ -1,21 +1,11 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 class BlogBase(BaseModel):
     title: str
     body: str
     class Config:
         orm_mode = True
-
-class BlogShow(BlogBase):
-    pass
-
-class BlogCreate(BlogBase):
-    pass
-
-class BlogUpdate(BlogBase):
-    title: Optional[str] = None
-    body: Optional[str] = None
 
 class UserBase(BaseModel):
     name: str
@@ -25,5 +15,16 @@ class UserBase(BaseModel):
 class UserShow(BaseModel):
     name: str
     email: str
+    blogs: List[BlogBase] = []
     class Config:
         orm_mode = True
+
+class BlogShow(BlogBase):
+    user: UserShow
+
+class BlogCreate(BlogBase):
+    pass
+
+class BlogUpdate(BlogBase):
+    title: Optional[str] = None
+    body: Optional[str] = None

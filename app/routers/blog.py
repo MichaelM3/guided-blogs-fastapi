e54a705595cda56f069867836b801344788a46e5
MichaelM3 <michael.muniz94@gmail.com>
2023-03-02 20:45:52 -0500

    added user auth onto blog routes

--- app/routers/blog.py
+++ app/routers/blog.py
@@ -11,21 +11,21 @@ router = APIRouter(
 )
 
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.BlogShow])
-def index(db: Session = Depends(database.get_db), get_current_user: schemas.UserBase = Depends(get_current_user)):
+def index(db: Session = Depends(database.get_db), current_user: schemas.UserBase = Depends(get_current_user)):
     return blog.get_all(db)
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.BlogShow)
-def show(id: int, db: Session = Depends(database.get_db)):
+def show(id: int, db: Session = Depends(database.get_db), current_user: schemas.UserBase = Depends(get_current_user)):
     return blog.show(id, db)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.BlogShow)
-def create(req: schemas.BlogCreate, db: Session = Depends(database.get_db)):
+def create(req: schemas.BlogCreate, db: Session = Depends(database.get_db), current_user: schemas.UserBase = Depends(get_current_user)):
     return blog.create(req, db)
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.BlogUpdate)
-def update(id: int, req: schemas.BlogUpdate, db: Session = Depends(database.get_db)):
+def update(id: int, req: schemas.BlogUpdate, db: Session = Depends(database.get_db), current_user: schemas.UserBase = Depends(get_current_user)):
     return blog.update(id, req, db)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(database.get_db)):
+def destroy(id: int, db: Session = Depends(database.get_db), current_user: schemas.UserBase = Depends(get_current_user)):
     return blog.destroy(id, db)
